b216e3052176154824be73056d4a35b2f4bdbd64
Brian Hurst <brian.hurst@gsa.gov>
2020-08-20 14:30:17 -0400

    Support multiple household members

--- index.js
+++ index.js
@@ -1,62 +1,78 @@
 require("dotenv").config();
 
-const admin = require("firebase-admin");
 const isCameraOn = require("is-camera-on");
 const { Client } = require("tplink-smarthome-api");
+
+// Init Firebase Relatime Database (if multiple users is enabled)
+let userRef = null;
+if (process.env.DB_USER) {
+  const admin = require("firebase-admin");
+
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      type: process.env.FIREBASE_TYPE,
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_AUTH_URI,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url:
+        process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+    }),
+    databaseURL: process.env.DB_URL
+  });
+
+  const db = admin.database();
+  userRef = db.ref("users");
+}
 
 // Init Kasa smart plug
 const client = new Client();
 const plug = client.getPlug({ host: process.env.DEVICE_IP });
 
-// Init Firebase Relatime Database
-admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url:
-      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
-  }),
-  databaseURL: process.env.DB_URL
-});
-const db = admin.database();
-const userRef = db.ref("users");
+const checkCamera = async state => {
+  const cameraState = await isCameraOn();
 
-async function check_camera(status) {
-  const camera_status = await isCameraOn();
-
-  if (camera_status && !status) {
-    status = true;
-    userRef.child(process.env.DB_USER).set(true);
-    plug.setPowerState(true);
+  if (cameraState && !state) {
+    state = setState(true);
   }
 
-  if (!camera_status && status) {
-    status = false;
-    userRef
-      .child(process.env.DB_USER)
-      .set(false)
-      .then(function() {
-        userRef.once("value").then(function(snapshot) {
-          Object.values(snapshot.val()).every(val => val !== true) &&
-            plug.setPowerState(false);
-        });
-      });
+  if (!cameraState && state) {
+    state = setState(false);
   }
 
   setTimeout(() => {
-    check_camera(status);
+    checkCamera(state);
   }, 5000);
-}
+};
 
-(async () => {
-  plug.setPowerState(false);
-  userRef.child(process.env.DB_USER).set(false);
-  check_camera(false);
-})();
+const setState = state => {
+  // Record state of multiple users in DB, otherwise set state of device directly
+  userRef ? persistCameraState(state) : setDeviceState(state);
+
+  return state;
+};
+
+const setDeviceState = state => {
+  plug.setPowerState(state).catch(e => console.log(e));
+};
+
+const persistCameraState = state => {
+  userRef
+    .child(process.env.DB_USER)
+    .set(state)
+    .then(function() {
+      !state
+        ? userRef.once("value").then(function(snapshot) {
+            Object.values(snapshot.val()).every(val => val !== true) &&
+              setDeviceState(state);
+          })
+        : setDeviceState(state);
+    });
+};
+
+// Set initial state
+checkCamera(setState(false));
